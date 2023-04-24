a34074d7d15bcdf5c679e9f3bfba446f9025ed03
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-24 14:16:07 +0300

    task 1 color-picker completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+
+function onStart() {
+  startEl.disabled = true;
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  changeColorTimerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  startEl.disabled = false;
+  clearInterval(changeColorTimerId);
+}
+
+startEl.addEventListener('click', onStart);
+stopEl.addEventListener('click', onStop);
