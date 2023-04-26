61233c4dbe03462933c761cb79aa11c69e7e4d8a
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-26 17:58:01 +0300

    Task 3 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,35 +1,53 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl = document.querySelector('.form');
-
 const delayEl = document.querySelector('[name="delay"]');
 const stepEl = document.querySelector('[name="step"]');
 const amountEl = document.querySelector('[name="amount"]');
 
-let delay = null;
-let step = null;
-let amount = null;
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        res({ position, delay });
+      } else {
+        // Reject
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
 
 function onSubmit(event) {
   event.preventDefault();
-  
-  delay = delayEl.value;
-  step = stepEl.value;
-  amount = amountEl.value;
-  
+
+  let delay = parseInt(delayEl.value);
+  const step = parseInt(stepEl.value);
+  const amount = parseInt(amountEl.value);
+
   // Показываем подсказку с введёнными значениями для удобства
-  Notiflix.Notify.info(`\n\ndelay: ${delay}, step: ${step}, amount: ${amount}`, {
-    timeout: 10000,
-  });
+  Notify.info(
+    `\n\ndelay: ${delay}ms, step: ${step}ms, amount: ${amount}`,
+    {
+      timeout: delay + step * amount + 3000,
+    }
+  );
+
+  for (let idx = 1; idx <= amount; idx++) {
+    createPromise(idx, delay); // Создаём промисы, передаём позицию
+    delay += step; // Увеличиаем задержку с заданным шагом
+  }
 
   // Очищаем поля формы
   delayEl.value = '';
