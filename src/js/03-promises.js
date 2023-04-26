4671262bea4df222c5bdf3290370d6b62d5be731
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-26 03:00:32 +0300

    Task 3 in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+let delay = null;
+let step = null;
+let amount = null;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +18,23 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+function onSubmit(event) {
+  event.preventDefault();
+  
+  delay = delayEl.value;
+  step = stepEl.value;
+  amount = amountEl.value;
+  
+  // Показываем подсказку с введёнными значениями для удобства
+  Notiflix.Notify.info(`\n\ndelay: ${delay}, step: ${step}, amount: ${amount}`, {
+    timeout: 10000,
+  });
+
+  // Очищаем поля формы
+  delayEl.value = '';
+  stepEl.value = '';
+  amountEl.value = '';
+}
+
+formEl.addEventListener('submit', onSubmit);
